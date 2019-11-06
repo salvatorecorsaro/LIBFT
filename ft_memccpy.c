c266e91e5865f475b5ec058ded583f18bd9e954e
scorsaro <scorsaro@c2r14s2.42madrid.com>
2019-11-06 11:55:36 +0100

    fixing norminette

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,23 +6,28 @@
 /*   By: scorsaro <scorsaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 18:08:31 by scorsaro          #+#    #+#             */
-/*   Updated: 2019/11/05 19:32:35 by scorsaro         ###   ########.fr       */
+/*   Updated: 2019/11/06 11:46:56 by scorsaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n)
-{
-    char            *dest;
-    char            *srce;
-    size_t          i;
+#include <stddef.h>
 
-    dest = dst;
-    srce = src;
-    i = -1;
-    if (dest && srce && c && n)
-        while (++i < n && src[i] != c)
-            dest[i] = src[i];
-        if (src[i] == c)
-            return (src[i + 1]);
-    return (0);
+void	*ft_memccpy(void *restrict dst,
+const void *restrict src, int c, size_t n)
+{
+	char	*dest;
+	char	*srce;
+	size_t	i;
+
+	dest = dst;
+	srce = src;
+	i = -1;
+	if (dest && srce && c && n)
+	{
+		while (++i < n && srce[i] != c)
+			dest[i] = srce[i];
+		if (srce[i] == c)
+			return (srce[i + 1]);
+	}
+	return (0);
 }
--- main.c
+++ main.c
@@ -1,28 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: scorsaro <scorsaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/05 17:40:12 by scorsaro          #+#    #+#             */
-/*   Updated: 2019/11/06 11:30:46 by scorsaro         ###   ########.fr       */
+/*   Created: 2019/11/06 11:38:39 by scorsaro          #+#    #+#             */
+/*   Updated: 2019/11/06 11:54:59 by scorsaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <string.h>
+#include <stdio.h>
 
-void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+void	*ft_memccpy(void *restrict dst,
+const void *restrict src, int c, size_t n)
 {
-	char			*dest;
-	const char		*srce;
-	size_t			i;
+	char	*dest;
+	char	*srce;
+	size_t	i;
 
 	dest = dst;
 	srce = src;
 	i = -1;
-	if (dest && srce && n)
-		while (++i < n)
+	if (dest && srce && c && n)
+	{
+		while (++i < n && srce[i] != c)
 			dest[i] = srce[i];
-	return (dst);
+		if (srce[i] == c)
+			return (srce[i + 1]);
+	}
+	return (0);
+}
+
+int		main(void)
+{
+	char	dest[50];
+	char	srce[50];
+
+	strcpy(dest, "hola mundo");
+	strcpy(srce, "hello world");
+	return (0);
 }
